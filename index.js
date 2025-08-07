d35914810931da96255737c5c2099f6b118c0049
Asir <mdasir017@gmaol.com>
2025-08-08 01:34:45 +0600

    first commit

---
+++ index.js
@@ -0,0 +1,19 @@
+
+
+const numbers = [ 747, 55, 655, 56, 588, 869, 88, 556, 5856, 88, 889, 89]
+console.log(numbers.length);
+console.log(numbers);
+
+// get element value by index
+console.log(numbers[4]);
+
+const fifth = numbers[4];
+console.log(fifth);
+
+
+// set or update element by index
+let abc = 54;
+abc = 88;
+
+numbers[1] = 345;
+console.log(numbers);
---
+++ lenght.js
@@ -0,0 +1,4 @@
+
+
+const numbers = [ 1, 646, 37, 647, 74, 326, 7374, 7337, 77, 737, 73];
+console.log(numbers.length);
