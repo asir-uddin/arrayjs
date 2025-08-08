93e2154de707eea009e7bac37c0121a60475c967
Asir <mdasir017@gmaol.com>
2025-08-08 15:48:59 +0600

    added the isArray rull of js

---
+++ indexof.js
@@ -0,0 +1,7 @@
+
+const name = ['anika', 'soniya', 'jhingalala', 'nibir', 'karim', 'labhlu'];
+
+
+
+console.log(name.indexOf('soniya'));
+console.log(name.indexOf('hobu'));
---
+++ isarray.js
@@ -0,0 +1,13 @@
+
+
+
+const friend = ['hablu', 'piano', 'apple', 'legend', 'brandi', 'vodka'];
+const num = [];
+const food = 'jinga la la lal';
+const age = 56;
+
+
+console.log(Array.isArray(friend));
+console.log(Array.isArray(num));
+console.log(Array.isArray(food));
+console.log(Array.isArray(age));
