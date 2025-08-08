e9a559cac039ebdd002c9d3d05fbfbcffe0beeec
Asir <mdasir017@gmaol.com>
2025-08-08 15:30:19 +0600

    added the last update

---
+++ includse.js
@@ -0,0 +1,13 @@
+
+
+const name = ['anika', 'soniya', 'jhingalala', 'nibir', 'karim', 'labhlu'];
+
+console.log(name);
+console.log(name.includes('jordge'));
+console.log(name.includes('anika'));
+
+if(name.includes('soniya')){
+    console.log('we can tour');
+}else{
+    console.log('we can not tour lalmonirhat');
+}
--- shift-unshift.js
+++ shift-unshift.js
@@ -2,5 +2,6 @@
 
 const human = ['abul', 'kabul', 'dilhi', 'dhaka', 'rangpur', 'nilphamari'];
 human.shift();
+console.log(human);
 human.unshift('anika');
 console.log(human);
